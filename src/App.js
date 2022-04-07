d092b5d61acc9770ef19dc4e9bffc310d9dc23b6
Sarah Diener <sdiener@ksu.edu>
2022-04-07 00:08:51 -0500

    Update- added fetch to backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
 import './App.css';
 
+
 function App() {
+  const [error,setError] = useState(null);
+
+
+  useEffect(()=>{
+    fetch("https://salty-lowlands-88611.herokuapp.com/db", {
+      method: "get",
+      headers: {
+        //"Content-Type": "application/json",
+        //"Access-Control-Allow-Credentials": true,
+      },
+      //credentials: "include",
+    })
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          //console.log(result);
+          try {
+            console.log("RESULT "+result.results[0].classification);
+          } catch (e) {
+            console.log("There was an error in try");
+            setError(e);
+          }
+          //console.log(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setError(error);
+          console.log("ERROR: "+error);
+        }
+      );
+  },[])
+
   return (
     <div className="App">
       <div>Hello world</div>
